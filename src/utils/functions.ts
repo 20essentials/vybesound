d9f637ed8bc0a4f3ae925d1c735c912c70a0f472
DominicDev <s0l0estudi4r@gmail.com>
2026-04-07 15:28:39 +0200

    v0.1.1

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -7,3 +7,15 @@ export const $$ = <T extends Element = HTMLElement>(el: string): T[] => [
 export function baseUrl(path: string) {
   return new URL(path.replace(/^\/+/, ''), import.meta.env.SITE).toString();
 }
+
+export const toKebabCase = (input = '') => {
+  return input
+    .normalize('NFD') // separa acentos
+    .replace(/[\u0300-\u036f]/g, '') // elimina acentos
+    .replace(/([a-z])([A-Z])/g, '$1-$2') // camelCase → camel-Case
+    .replace(/[_\s]+/g, '-') // espacios y underscores → -
+    .replace(/[^a-zA-Z0-9-]/g, '') // elimina caracteres especiales
+    .replace(/--+/g, '-') // evita dobles guiones
+    .toLowerCase()
+    .trim();
+};
