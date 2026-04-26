8b07b316515ab368e5d87962427ba643deca5a2f
DominicDev <s0l0estudi4r@gmail.com>
2026-04-26 15:59:28 +0200

    v0.3.6

---
+++ src/utils/performers/ptasinski.ts
@@ -0,0 +1,28 @@
+import type { ArrayItemPerformer } from '../types';
+
+export const PTASINSKI: ArrayItemPerformer = [
+  'PTASINSKI',
+  'https://20essentials.github.io/project-000-002/assets/pt4sinski',
+  [
+    'LIGHT' /*1*/,
+    'DEFECTRUM' /*2*/,
+    'HEAVENLY AUDIO (GUITAR VERSION)' /*3*/,
+    'GRADUATION' /*4*/,
+    'EXODUS' /*5*/,
+    'TRUST' /*6*/,
+    'IRATE' /*7*/,
+    'HOT YOGA' /*8*/,
+    'GUY FIERI' /*9*/,
+    'DIAREA' /*10*/,
+    'TRIBULATIONS' /*11*/,
+    'PREGNANT' /*12*/,
+    'MOCHI' /*13*/,
+    'SYBAU' /*14*/,
+    'IDK U TELL ME' /*15*/,
+    'FEET' /*16*/,
+    'SAITAMA' /*17*/,
+    'URANUS' /*18*/,
+    'ERWIN' /*19*/,
+    'HOLLUP' /*20*/
+  ]
+];
